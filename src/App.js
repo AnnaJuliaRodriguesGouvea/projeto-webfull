32e7271f706820368c6371629f918336a2add70a
Anna Rodrigues <annagouvea@alunos.utfpr.edu.br>
2023-10-19 19:24:28 -0300

    Inicializando projeto React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import ListaFrutas from './components/ListaFrutas';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <ListaFrutas/>
+    </div>
+  );
+}
+
+export default App;
